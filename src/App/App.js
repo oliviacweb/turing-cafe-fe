93b291cd83d435a3610a7b1ace12cc852f6b5612
Olivia Webster <oliviacweb@gmail.com>
2020-05-21 11:42:46 -0400

    Add reservation container component with initial message and render on app

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReservationContainer from '../ReservationContainer/ReservationContainer'
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+        <ReservationContainer />
+
         </div>
       </div>
     )
---
+++ src/ReservationContainer/ReservationContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ReservationContainer extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return(
+      <h2>reservation cards go here </h2>
+    )
+  }
+}
+
+export default ReservationContainer;
